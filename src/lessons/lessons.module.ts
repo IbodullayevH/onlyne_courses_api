af2e6fda771bbd86420fce794a875dd0e6c8f298
Ibodullayev_Hasan <ibodullayevh040@gmail.com>
2024-10-27 12:38:22 +0500

    Lessons.entitiy add

--- src/lessons/lessons.module.ts
+++ src/lessons/lessons.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { LessonsService } from './lessons.service';
 import { LessonsController } from './lessons.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './entities/lesson.entity';
+import { Modules } from 'src/modules/entities/module.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Lesson, Modules]),
+    AuthModule
+  ],
   controllers: [LessonsController],
   providers: [LessonsService],
 })
-export class LessonsModule {}
+export class LessonsModule { }
