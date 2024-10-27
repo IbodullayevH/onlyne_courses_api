76a14ab67d1f75318233d9beb5305389fe63eac2
Ibodullayev_Hasan <ibodullayevh040@gmail.com>
2024-10-27 12:48:49 +0500

    CreateLessonDto and  UpdateLessonDto methods fixed

--- src/lessons/dto/create-lesson.dto.ts
+++ src/lessons/dto/create-lesson.dto.ts
@@ -1,1 +1,24 @@
-export class CreateLessonDto {}
+import { IsNotEmpty, IsString, IsEnum, IsOptional, IsInt, IsUrl } from 'class-validator';
+import { LessonType } from '../entities/lesson.entity';
+
+export class CreateLessonDto {
+    @IsNotEmpty()
+    @IsString()
+    title: string;
+
+    @IsOptional()
+    @IsString()
+    content?: string;
+
+    @IsNotEmpty()
+    @IsEnum(LessonType)
+    type: LessonType;
+
+    @IsOptional()
+    @IsUrl()
+    videoUrl?: string;
+
+    @IsNotEmpty()
+    @IsInt()
+    moduleId: number;
+}
